516c2514465c3b0b62a985f56edb7932016ec2fe
nin-jin <nin-jin@users.noreply.github.com>
2020-02-24 18:56:12 +0000

    Deployed using Blazing fast GitHub Pages deploy

---
+++ list/mol/-css/mol.view.css.ts
@@ -0,0 +1,3 @@
+namespace $ { $mol_style_attach( "hyoo/bench/list/mol/mol.view.css",
+ "[hyoo_bench_list_mol] {\n\tpadding: .5rem;\n}\n\n[hyoo_bench_list_mol_head] {\n\tpadding: .5rem;\n\tmargin: 0;\n\tfont-size: 1.5em;\n\tfont-weight: normal;\n}\n\n[hyoo_bench_list_mol_row] {\n\tpadding: .5rem;\n\tcursor: pointer;\n\tdisplay: block;\n}\n\n[hyoo_bench_list_mol_row]:hover {\n\tbox-shadow: none;\n}\n\n[hyoo_bench_list_mol_row][mol_check_checked] {\n\tbackground: #eee;\n}\n\n[hyoo_bench_list_mol_row_title] {\n\tfont-weight: bold;\n\tdisplay: block;\n}\n\n[hyoo_bench_list_mol_row_content] {\n\tdisplay: block;\n}\n"
+) }
