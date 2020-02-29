1226e937d76f15d362fe8d0ff5b054255097dae5
nin-jin <nin-jin@users.noreply.github.com>
2020-02-29 19:23:52 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- chart/bar/mol/-css/mol.view.css.ts
+++
@@ -1,3 +0,0 @@
-namespace $ { $mol_style_attach( "hyoo/bench/chart/bar/mol/mol.view.css",
- "[hyoo_bench_chart_bar_mol] {\n\tdisplay: flex;\n}\n"
-) }
--- chart/rope/mol/-css/mol.view.css.ts
+++
@@ -1,3 +0,0 @@
-namespace $ { $mol_style_attach( "hyoo/bench/chart/rope/mol/mol.view.css",
- "[hyoo_bench_chart_rope_mol] {\n\tdisplay: flex;\n}\n"
-) }
