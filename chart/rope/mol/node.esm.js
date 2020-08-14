c4e85ab7969c8e73e8b610012d77b5f53e9a7a29
zerkalica <zerkalica@users.noreply.github.com>
2020-08-14 07:11:44 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- chart/rope/mol/node.esm.js
+++ chart/rope/mol/node.esm.js
@@ -3392,6 +3392,7 @@ var $;
                 grow: 1,
                 shrink: 1,
             },
+            outline: 'none',
             alignSelf: 'stretch',
             boxSizing: 'border-box',
             willChange: 'scroll-position',
--- chart/rope/mol/web.js
+++ chart/rope/mol/web.js
@@ -3092,6 +3092,7 @@ var $;
                 grow: 1,
                 shrink: 1,
             },
+            outline: 'none',
             alignSelf: 'stretch',
             boxSizing: 'border-box',
             willChange: 'scroll-position',
