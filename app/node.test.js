bcdfd7bec4d220b5cd239933ac072bb17225653f
zerkalica <zerkalica@users.noreply.github.com>
2020-08-16 07:11:47 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- app/node.test.js
+++ app/node.test.js
@@ -5928,6 +5928,12 @@ var $;
                 color: $.$mol_theme.hover,
             }
         },
+        ':focus-within': {
+            outline: 'none',
+            background: {
+                color: $.$mol_theme.hover,
+            }
+        },
         '@': {
             mol_link_current: {
                 'true': {
