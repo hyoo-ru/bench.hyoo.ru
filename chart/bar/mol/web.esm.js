044abe47b02ccbe027fed9f03c056488962b3246
nin-jin <nin-jin@users.noreply.github.com>
2021-06-14 07:11:31 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- chart/bar/mol/web.esm.js
+++ chart/bar/mol/web.esm.js
@@ -4203,6 +4203,9 @@ var $;
     (function ($$) {
         const { per, rem, px } = $.$mol_style_unit;
         $.$mol_style_define($$.$mol_scroll, {
+            overflow: 'auto',
+        });
+        $.$mol_style_define($$.$mol_scroll, {
             display: 'flex',
             overflow: 'overlay',
             flex: {
