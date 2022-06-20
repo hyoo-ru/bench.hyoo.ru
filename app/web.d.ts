1bad1cc30bd9087086f88780e69a6a39a2d3e701
nin-jin <nin-jin@users.noreply.github.com>
2022-06-20 07:14:33 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- app/web.d.ts
+++ app/web.d.ts
@@ -2290,6 +2290,7 @@ declare namespace $.$$ {
 
 declare namespace $ {
     class $mol_embed_native extends $mol_scroll {
+        uri(val?: any): string;
         dom_name(): string;
         window(): any;
         attr(): {
@@ -2297,8 +2298,10 @@ declare namespace $ {
             type: string;
         };
         sub(): readonly any[];
-        uri(val?: any): string;
+        uri_object(): string;
         mime(): string;
+        uri_link(): string;
+        uri_image(): string;
         title(val?: any): string;
         Fallback_image(): $mol_image;
         Fallback_link(): $$.$mol_link;
@@ -2333,6 +2336,7 @@ declare namespace $.$$ {
         uri_listener(): $mol_dom_listener;
         uri_change(event?: MessageEvent<[string, string]>): void;
         auto(): (Window | $mol_dom_listener)[];
+        uri_object(): string;
     }
 }
 
@@ -2356,6 +2360,7 @@ declare namespace $ {
         Code_line(id: any): $$.$mol_text_code_row;
         Link(id: any): $$.$mol_link_iconed;
         Link_http(id: any): $$.$mol_link_iconed;
+        Image_fallback(id: any): $mol_image;
         Image(id: any): $$.$mol_embed_native;
         auto_scroll(): any;
         block_content(id: any): readonly any[];
