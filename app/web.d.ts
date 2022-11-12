40e13077ee51084c2cac887c56ee312c5043e33e
nin-jin <nin-jin@users.noreply.github.com>
2022-11-12 07:14:32 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- app/web.d.ts
+++ app/web.d.ts
@@ -465,6 +465,10 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    function $mol_wire_solid(): void;
+}
+
+declare namespace $ {
     function $mol_const<Value>(value: Value): {
         (): Value;
         '()': Value;
@@ -2351,10 +2355,6 @@ declare namespace $ {
         Fallback(): $$.$mol_link;
         uri_change(next?: any): any;
     }
-}
-
-declare namespace $ {
-    function $mol_wire_solid(): void;
 }
 
 declare namespace $ {
