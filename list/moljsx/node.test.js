1ca1ae5defbbd094f0c478433515a4f0293e324b
nin-jin <nin-jin@users.noreply.github.com>
2023-07-12 07:12:51 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- list/moljsx/node.test.js
+++ list/moljsx/node.test.js
@@ -2118,6 +2118,7 @@ var $;
             }
         };
         Reflect.defineProperty(descr2.value, 'name', { value: orig.name + ' ' });
+        Reflect.defineProperty(descr2.value, 'length', { value: orig.length });
         Object.assign(descr2.value, { orig });
         Reflect.defineProperty(host, field, descr2);
         return descr2;
@@ -2155,6 +2156,7 @@ var $;
             }
         };
         Reflect.defineProperty(descr2.value, 'name', { value: orig.name + ' ' });
+        Reflect.defineProperty(descr2.value, 'length', { value: orig.length });
         Object.assign(descr2.value, { orig });
         Reflect.defineProperty(host, field, descr2);
         return descr2;
