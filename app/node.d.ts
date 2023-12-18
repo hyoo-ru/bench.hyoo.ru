afa53ef4bd2a3440e5c98f65428c6e2506b2a9b2
nin-jin <nin-jin@users.noreply.github.com>
2023-12-18 07:11:47 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- app/node.d.ts
+++ app/node.d.ts
@@ -1987,6 +1987,7 @@ declare namespace $ {
         target(): string;
         file_name(): string;
         current(): boolean;
+        relation(): string;
         event_click(event?: any): any;
         click(event?: any): any;
     }
--- app/node.test.js
+++ app/node.test.js
@@ -6510,7 +6510,8 @@ var $;
                 title: this.hint_safe(),
                 target: this.target(),
                 download: this.file_name(),
-                mol_link_current: this.current()
+                mol_link_current: this.current(),
+                rel: this.relation()
             };
         }
         sub() {
@@ -6544,6 +6545,9 @@ var $;
         }
         current() {
             return false;
+        }
+        relation() {
+            return "";
         }
         event_click(event) {
             if (event !== undefined)
