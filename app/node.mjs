0ab8acfa6e0677836a4e6fd705dc9930059eb8aa
nin-jin <nin-jin@users.noreply.github.com>
2025-05-05 07:21:05 +0000

    Deploying to gh-pages from @ hyoo-ru/bench.hyoo.ru@efabb4d93d4424086fe73afdf2d0690b88a0ece4 🚀

--- app/node.mjs
+++ app/node.mjs
@@ -10338,7 +10338,7 @@ var $;
 			return obj;
 		}
 		type(){
-			return "tel";
+			return "text";
 		}
 		value_string(next){
 			if(next !== undefined) return next;
@@ -10357,6 +10357,7 @@ var $;
 		String(){
 			const obj = new this.$.$mol_string();
 			(obj.type) = () => ((this.type()));
+			(obj.keyboard) = () => ("decimal");
 			(obj.value) = (next) => ((this.value_string(next)));
 			(obj.hint) = () => ((this.hint()));
 			(obj.enabled) = () => ((this.string_enabled()));
