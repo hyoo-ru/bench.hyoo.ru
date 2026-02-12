89a701074d104d0ab31d68bffd4b93fc23254f16
nin-jin <nin-jin@users.noreply.github.com>
2026-02-12 07:53:08 +0000

    Deploying to gh-pages from @ hyoo-ru/bench.hyoo.ru@efabb4d93d4424086fe73afdf2d0690b88a0ece4 🚀

--- list/moljsx/web.d.ts
+++ list/moljsx/web.d.ts
@@ -114,6 +114,11 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    const $mol_key_handle: unique symbol;
+    const $mol_key_store: WeakMap<object, string>;
+}
+
+declare namespace $ {
     class $mol_object2 {
         static $: $;
         [Symbol.toStringTag]: string;
@@ -124,6 +129,7 @@ declare namespace $ {
         static [Symbol.toPrimitive](): any;
         static toString(): any;
         static toJSON(): any;
+        static [$mol_key_handle](): any;
         destructor(): void;
         static destructor(): void;
         [Symbol.dispose](): void;
@@ -294,7 +300,6 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    const $mol_key_store: WeakMap<object, string>;
     function $mol_key<Value>(value: Value): string;
 }
 
