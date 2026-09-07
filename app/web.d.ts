e8e14fbfe5f9da6d489cd663219592f1f0515b7f
nin-jin <nin-jin@users.noreply.github.com>
2026-09-07 13:19:35 +0000

    Deploying to gh-pages from @ hyoo-ru/bench.hyoo.ru@efabb4d93d4424086fe73afdf2d0690b88a0ece4 🚀

--- app/web.d.ts
+++ app/web.d.ts
@@ -5161,6 +5161,7 @@ declare namespace $.$$ {
         sub(): ($.$mol_string | $mol_button_minor)[];
         value_limited(val?: number): number;
         event_dec(next?: Event): void;
+        precision_change(): number;
         event_inc(next?: Event): void;
         event_dec_boost(next?: Event): void;
         event_inc_boost(next?: Event): void;
